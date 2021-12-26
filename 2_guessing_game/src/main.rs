15e9dc82658915209b6cd586904aa7780801c88e
David Dunn <26876072+doubledup@users.noreply.github.com>
2021-12-26 15:21:46 +0200

    Generate random numbers

--- 2_guessing_game/src/main.rs
+++ 2_guessing_game/src/main.rs
@@ -1,9 +1,17 @@
 use std::io;
+use rand::Rng;
+use rand::distributions::Uniform;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    let val = rand::thread_rng().sample(Uniform::new_inclusive(1, 100));
+    println!("A uniformly random number is: {}", val);
 
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
