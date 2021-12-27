f4880929ff7f3cb93cca20b3902f94a171eb9d67
David Dunn <26876072+doubledup@users.noreply.github.com>
2021-12-27 19:43:19 +0200

    Compare guess to secret number

--- 2_guessing_game/src/main.rs
+++ 2_guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use rand::Rng;
 use rand::distributions::Uniform;
+use std::cmp::Ordering;
 
 fn main() {
     let val = rand::thread_rng().sample(Uniform::new_inclusive(1, 100));
@@ -17,5 +18,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
