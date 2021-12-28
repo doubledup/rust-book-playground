1500879f605579159d3e53b96ac34c18bc619793
David Dunn <26876072+doubledup@users.noreply.github.com>
2021-12-28 19:06:02 +0200

    Add control flow section

---
+++ 3.5_control_flow/src/main.rs
@@ -0,0 +1,101 @@
+fn main() {
+    // loops
+    loop {
+        println!("again!");
+        break;
+    }
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    println!("using while:");
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is {}", a[index]);
+        index += 1;
+    }
+
+    println!("using for:");
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("The value is {}", element);
+    }
+
+    // countdown using a range
+    for number in (1..=3).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    println!("_______________________________________________________________________");
+
+    // if expressions
+    let number = 7;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // if number {
+    //     println!("number was three");
+    // }
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3 or 2")
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
+
+    // let condition = true;
+    // let number = if condition { 5 } else { "six" };
+    // println!("The value of number is: {}", number);
+}
