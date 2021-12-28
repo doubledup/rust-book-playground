2992efe7877e77e8a5a66a048d4e9eb03e11817b
David Dunn <26876072+doubledup@users.noreply.github.com>
2021-12-28 11:54:06 +0200

    Add variables section

---
+++ 3.1_variables/src/main.rs
@@ -0,0 +1,22 @@
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    let mut x = 5;
+    let y = 14;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    x = 6;
+    let y = 42;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+
+    let z = 7;
+    let z = z + 1;
+
+    {
+        let z = z * 2;
+        println!("The value of z in the inner scope is: {}", z);
+    }
+
+    println!("The value of z is: {}", z);
+}
