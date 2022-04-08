9240646f7997c1daec664f3aecc5a925936fb2b5
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-04-08 15:07:42 +0200

    Use a constant

--- 3.1_variables/src/main.rs
+++ 3.1_variables/src/main.rs
@@ -1,4 +1,4 @@
-// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
     let mut x = 5;
@@ -9,6 +9,12 @@ fn main() {
     let y = 42;
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+
+    println!("---");
+
+    println!("Three hours contains {} seconds", THREE_HOURS_IN_SECONDS);
+
+    println!("---");
 
     let z = 7;
     let z = z + 1;
