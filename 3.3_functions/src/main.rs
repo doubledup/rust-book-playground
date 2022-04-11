346b4b63f729bb566675fd69216c9a947634e999
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-04-11 12:03:05 +0200

    Update functions section

--- 3.3_functions/src/main.rs
+++ 3.3_functions/src/main.rs
@@ -1,9 +1,27 @@
-fn main() {
-     let z = plus_one(5);
-     println!("The value of z is: {}", z);
+fn another_function() {
+    println!("Another function.")
+}
 
-    let x = five();
-    println!("The value of x is: {}", x);
+fn main() {
+    println!("Hello, world!");
+    another_function();
+
+    println!("---");
+
+    print_value(5);
+
+    println!("---");
+
+    print_labeled_measurement(5, 'h');
+
+    println!("---");
+
+    // let x = (let y = 6);
+    let _answer: u8 = 42;
+    let answer_is_42 = matches!(42, _answer);
+    println!("Is the answer 42? {}", if answer_is_42 { "Yes" } else { "No" });
+
+    println!("---");
 
     let y = {
         let x = 3;
@@ -11,31 +29,28 @@ fn main() {
     };
     println!("The value of y is: {}", y);
 
-    // let x = (let y = 6);
+    println!("---");
 
-    print_labeled_measurement(5, 'h');
+    let x = five();
+    println!("The value of x is: {}", x);
 
-    print_value(5);
-
-    another_function();
+    let x = plus_one(x);
+    println!("The value of x is now: {}", x);
 }
 
-fn plus_one(x: i32) -> i32 {
-    return x + 1;
-}
-
-fn five() -> i32 {
-    5
+fn print_value(x: u8) {
+    println!("The value of x is: {}", x);
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {}{}", value, unit_label);
 }
 
-fn print_value(x: i32) {
-    println!("The value of x is: {}", x);
+fn five() -> i32 {
+    // return can be an expression, not just a statement:
+    return 5
 }
 
-fn another_function() {
-    println!("Another function.")
+fn plus_one(x: i32) -> i32 {
+    return x + 1;
 }
